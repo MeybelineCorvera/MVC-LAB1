2280b61f6f822c23557faefa4331fee62c967dc8
MeybelineCorvera <ccabigai48l@gmail.com>
2025-02-26 19:10:25 -0600

    Add project files.

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,35 @@
+using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using MVC.Db;
+using MVC.Models;
+
+namespace MVC.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+        private readonly AppDbContext appDbContext;
+        public HomeController(ILogger<HomeController> logger, AppDbContext  _dbContext )
+        {
+            _logger = logger;
+            appDbContext = _dbContext;
+        }
+
+        public IActionResult Index()
+        {
+            appDbContext.Alumnos.ToList(); 
+            return View(Alumnos);
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
