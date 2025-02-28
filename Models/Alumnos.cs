347fd6ab118bbd723ac786ff04864a1df202dcf9
MeybelineCorvera <ccabigai48l@gmail.com>
2025-02-27 22:08:03 -0600

    CRUD insertCRUD-1

--- Models/Alumnos.cs
+++ Models/Alumnos.cs
@@ -10,22 +10,35 @@ namespace MVC.Models
         [Key ]
         public int AlumnoId { get; set; }
         [NotNull]
+        [Required(ErrorMessage= "Nombre es requerido")]
         public string  Nombres { get; set; }
         [NotNull]
+        [Required(ErrorMessage = "Apellidos es requerido")]
         public string Apellidos { get; set; }
         [NotNull]
+        [Required(ErrorMessage = "Edad es requerido")]
         public int Edad { get; set; }
         [NotNull]
+        [Required(ErrorMessage = "Carnet es requerido")]
         public string Carnet { get; set;}
+        [Required(ErrorMessage = "Cantidad de materias es requerido")]
         public int CantidadMaterias { get; set; }
+        [Required(ErrorMessage = "Ciclo es requerido")]
         public int Ciclo { get; set; }
+        [Required(ErrorMessage = "Fecha de nacimiento es requerido")]
         public DateTime FechaNacimiento { get; set; }
         [NotMapped]
         public string NombreCompleto { get; set; }
         #endregion Propiedades
 
         #region Constructor
-        public Alumnos() { }
+        public Alumnos() 
+        {
+            FechaNacimiento = DateTime.Now;
+            IsActive = true;
+            Created = DateTime.Now;
+            CreatedBy = "ADMIN";
+        }
         public Alumnos(string nombres, int edad, string apellidos, DateTime fechaNacimiento)
         {
             Nombres = nombres;
